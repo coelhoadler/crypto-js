3ddcf8af4fc33a399f72327fe059bf7e7be18754
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2021-04-18 12:22:41 -0300

    feat: (wip) interceptando função writeFile

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const app = require('./src/app.js')
+const CryptoHelper = require('./src/cryptoHelper.js');
+const CustomFSPromises = require('./src/customFSPromises.js');
+const Decorator = require('./src/decorator.js');
+
 
 ;(async () => {
+    const config = {
+        cryptoKey: 'adler+coelho+santos@2021'
+    }
+    
+    // const enc = await crypto.encrypt('Hello World');
+    // const decrypto = await crypto.decrypt(enc);
+
+    const cryptoHelper = await CryptoHelper.setup(config);
+    const customFSPromises = new CustomFSPromises({ cryptoHelper }).configure();
+
+    Decorator.decorateModule(customFSPromises, require('fs').promises)
+
     await app.run();
 })()
